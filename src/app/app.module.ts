e5338c722111d7bb54556014a1263e8fda069e46
gharseldin <gharseldin@aucegypt.edu>
2018-12-10 07:02:35 -0600

    ngx-graph setup
    ngx-graph setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxChartsModule,
+    NgxGraphModule
   ],
   providers: [],
   bootstrap: [AppComponent]
