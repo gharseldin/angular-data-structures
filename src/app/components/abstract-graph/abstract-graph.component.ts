aa1552e5112b9763a8c8880ca695beb75e12ad88
gharseldin <gharseldin@aucegypt.edu>
2018-12-23 17:03:07 -0600

    updated directive binding for SinglyLinkedList
    -updated directive binding for SinglyLinkedList

--- src/app/components/abstract-graph/abstract-graph.component.ts
+++ src/app/components/abstract-graph/abstract-graph.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as shape from 'd3-shape';
+import { Input } from '@angular/core';
+import { Model } from '../../../model/Model'
 
 @Component({
   selector: 'app-abstract-graph',
@@ -8,73 +10,33 @@ import * as shape from 'd3-shape';
 })
 export class AbstractGraphComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  testModel: Model;
+
+  constructor() {
+
+   }
 
   ngOnInit() {
+    this.nodes = this.testModel.getNodes();
+    this.links = this.testModel.getLinks();
   }
 
   curve: any = shape.curveLinear;
-    view: any[];
-    autoZoom: boolean = false;
-    panOnZoom: boolean = true;
-    enableZoom: boolean = true;
-    autoCenter: boolean = false;
-    showLegend: boolean = false;
-    colorScheme: any = {
-        domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-    };
+  view: any[];
+  autoZoom: boolean = false;
+  panOnZoom: boolean = true;
+  enableZoom: boolean = true;
+  autoCenter: boolean = false;
+  showLegend: boolean = false;
+  colorScheme: any = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
 
-    nodes: any[] = [
-        {
-            id: 'start',
-            label: 'start'
-        }, {
-            id: '1',
-            label: 'Query ThreatConnect',
-        }, {
-            id: '2',
-            label: 'Query XForce',
-        }, {
-            id: '3',
-            label: 'Format Results'
-        }, {
-            id: '4',
-            label: 'Search Splunk'
-        }, {
-            id: '5',
-            label: 'Block LDAP'
-        }, {
-            id: '6',
-            label: 'Email Results'
-        }
-    ];
+  nodes: any[];
+  links: any[];
 
-    links: any[] = [
-        {
-            source: 'start',
-            target: '1',
-            label: 'links to'
-        }, {
-            source: 'start',
-            target: '2'
-        }, {
-            source: '1',
-            target: '3',
-            label: 'related to'
-        }, {
-            source: '2',
-            target: '4'
-        }, {
-            source: '2',
-            target: '6'
-        }, {
-            source: '3',
-            target: '5'
-        }
-    ];
-
-    select(event) {
-        console.log('selected');
-    }
-
+  select(event) {
+    console.log('selected');
+  }
 }
--- src/app/components/singly-linked-list/singly-linked-list.component.ts
+++ src/app/components/singly-linked-list/singly-linked-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { SinglyLinkedList } from '../../../model/linkedlists/SinglyLinkedList';
+import { SinglyLinkedList } from 'src/model/linkedlists/SinglyLinkedList';
+import { Model } from 'src/model/Model';
 
 @Component({
   selector: 'app-singly-linked-list',
@@ -8,12 +9,84 @@ import { SinglyLinkedList } from '../../../model/linkedlists/SinglyLinkedList';
 })
 export class SinglyLinkedListComponent implements OnInit {
 
-  constructor() { }
+  linkedList: SinglyLinkedList<string>;
+  singlyLinkedListModel: Model;
+  test: string = "1234";
+
+  constructor() {
+    this.linkedList = new SinglyLinkedList<string>();
+
+    this.singlyLinkedListModel = new Model(
+      [
+        {
+          id: 'start',
+          label: 'start'
+        }, {
+          id: '1',
+          label: 'Query ThreatConnect',
+        }, {
+          id: '2',
+          label: 'Query XForce',
+        }, {
+          id: '3',
+          label: 'Format Results'
+        }, {
+          id: '4',
+          label: 'Search Splunk'
+        }, {
+          id: '5',
+          label: 'Block LDAP'
+        }, {
+          id: '6',
+          label: 'Email Results'
+        }
+      ],
+      [
+        {
+          source: 'start',
+          target: '1',
+          label: 'links to'
+        },
+        {
+          source: '1',
+          target: 'start',
+          label: 'links to'
+        },
+        {
+          source: 'start',
+          target: '2'
+        }, {
+          source: '1',
+          target: '3',
+          label: 'related to'
+        }, {
+          source: '2',
+          target: '4'
+        }, {
+          source: '2',
+          target: '6'
+        }, {
+          source: '3',
+          target: '5'
+        }
+      ]
+    );
+    this.singlyLinkedListModel.addNode({
+      id: '7',
+      label: 'Test'
+    });
+    this.singlyLinkedListModel.addLink({
+      source: '7',
+      target: '3'
+    })
+    console.log("model : " + this.singlyLinkedListModel);
+  }
 
   ngOnInit() {
   }
+  singlyLinkedList: SinglyLinkedList<string>;
 
-  singlyLinkedList : SinglyLinkedList<string>;
-
-
+  refreshModel():void {
+    
+  }
 }
