7d8e3af5dccbb1150e0fc2f2441a2fc551c972e9
Amr Gharseldin <agharse@us.ibm.com>
2018-12-30 16:13:07 -0600

    General Formatting of the code
    General Formatting of the code

--- src/model/stacks/ArrayStack.ts
+++ src/model/stacks/ArrayStack.ts
@@ -1,33 +1,35 @@
 import { Stack } from "./Stack";
-export class ArrayStack<E> implements Stack<E>{
+export class ArrayStack<E> implements Stack<E> {
 
-    private data:E[];
-    private t:number = -1;
-    
-    constructor(){ }
+    private data: E[];
+    private t = -1;
 
-    public size():number {
-        return this.t+1;
+    constructor() { }
+
+    public size(): number {
+        return this.t + 1;
     }
 
-    public isEmpty():boolean {
-        return this.t==-1;
+    public isEmpty(): boolean {
+        return this.t === -1;
     }
 
-    public push(e:E):void{
+    public push(e: E): void {
         this.data[++this.t] = e;
     }
 
-    public top():E {
-        if(this.isEmpty())
+    public top(): E {
+        if (this.isEmpty()) {
             return null;
+        }
         return this.data[this.t];
     }
 
-    public pop():E{
-        if(this.isEmpty())
+    public pop(): E {
+        if (this.isEmpty()) {
             return null;
-        const answer:E = this.data[this.t];
+        }
+        const answer: E = this.data[this.t];
         this.data[this.t] = null;
         this.t--;
         return answer;
